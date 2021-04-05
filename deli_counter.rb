896b1ee1f13043e189b15b2a885790e32129b7ec
Robyn Taylor <rhtaylor0715@gmail.com>
2021-04-05 14:53:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+katz_deli = []
+
+#shows everyone their place in line
+def line(array)
+  current_line = []
+  if array.empty?
+    puts "The line is currently empty."
+  else !array.empty?
+    array.each_with_index.do |name, index|
+      current_line.push("#{index}. #{name}")
+    end  
+    puts "The line is currently: #{current_line.join(" ")}"
+  end
+end
+
+
+
+
+#puts a name and position number in line
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+
+
+
+
+#puts next person in line (i.e. first element in array) and removes their name from the array
+def now_serving
+  puts
+end
