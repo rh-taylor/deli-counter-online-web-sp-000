cc52bd5f7c830676eb29738ffe358489622b5140
Robyn Taylor <rhtaylor0715@gmail.com>
2021-04-05 15:12:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,7 +27,7 @@ def now_serving(array)
   if array.empty?
     puts "There is nobody waiting to be served!"
   else !array.empty?
-    puts "Currently serving, #{array[0]}."
+    puts "Currently serving #{array[0]}."
     array.shift
   end
 end
